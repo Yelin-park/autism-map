72e378eac7c8f2067fbb781b7b13d29d53c82a85
Yaliny <mae1217@naver.com>
2025-06-10 15:09:38 +0900

    init

---
+++ src/test/java/com/yaliny/autismmap/AutismMapApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yaliny.autismmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutismMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("GOOD");
+	}
+
+}
