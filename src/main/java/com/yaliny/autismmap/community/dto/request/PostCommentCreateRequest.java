adf21ffe6e334ed347804e816ac0f59915672f35
Yaliny <mae1217@naver.com>
2025-07-17 23:04:57 +0900

    docs(커뮤니티 기능): 댓글 등록 Reuqest에 @Schema 추가하여 설명 작성

--- src/main/java/com/yaliny/autismmap/community/dto/request/PostCommentCreateRequest.java
+++ src/main/java/com/yaliny/autismmap/community/dto/request/PostCommentCreateRequest.java
@@ -1,8 +1,13 @@
 package com.yaliny.autismmap.community.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PostCommentCreateRequest(
+    @Schema(title = "사용자 ID", description = "사용자 ID")
     long memberId,
+    @Schema(title = "댓글 내용", description = "댓글 내용")
     String content,
+    @Schema(title = "대댓글 작성 시 부모 댓글 ID", description = "대댓글 작성 시 부모 댓글 ID")
     Long parentCommentId
 ) {
 }
