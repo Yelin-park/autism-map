24a646b97c2279fbb719828718b2f9051b46f31e
Yaliny <mae1217@naver.com>
2025-07-27 21:22:28 +0900

    feat(OAuth 기능): 소셜 로그인 중복 이메일 예외 처리 완료

---
+++ src/main/java/com/yaliny/autismmap/global/config/OAuth2UserConfig.java
@@ -0,0 +1,13 @@
+package com.yaliny.autismmap.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+
+@Configuration
+public class OAuth2UserConfig {
+    @Bean
+    public DefaultOAuth2UserService defaultOAuth2UserService() {
+        return new DefaultOAuth2UserService();
+    }
+}
