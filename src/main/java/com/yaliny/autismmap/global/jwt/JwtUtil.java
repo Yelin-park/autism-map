970ce7647290c048cd695cfa0725c0bc4d459d46
Yaliny <mae1217@naver.com>
2025-08-05 15:17:49 +0900

    refactor(nurean): final 추가, 주입 방식 변경, 로그 추가, 트랜잭션 수정
    - SecurityConfig의 ObjectMapper 필드 주입 방식으로 변경
    - LoggingIntercpetor 클래스 불변 선언
    - 토큰 유효성 검사 실패 시 로그 기록 및 토큰 유효성 검증 로그 기록
    - 라인 정리

--- src/main/java/com/yaliny/autismmap/global/jwt/JwtUtil.java
+++ src/main/java/com/yaliny/autismmap/global/jwt/JwtUtil.java
@@ -1,12 +1,10 @@
 package com.yaliny.autismmap.global.jwt;
 
 import com.yaliny.autismmap.member.entity.Role;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +15,7 @@ import java.util.Date;
  * JWT 토큰 생성, 검증, 토큰에서 사용자 정보 꺼내는 역할을 한다.
  */
 @Component
+@Slf4j
 public class JwtUtil {
 
     @Value("${jwt.secret}")
@@ -51,11 +50,20 @@ public class JwtUtil {
      */
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            getClaims(token); // 파싱이 가능해야 유효한 토큰
             return true;
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 JWT 토큰: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("지원하지 않는 JWT 토큰: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("잘못된 JWT 구조: {}", e.getMessage());
+        } catch (SignatureException e) {
+            log.error("서명 오류 : {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims 비어있음 : {}", e.getMessage());
         }
+        return false;
     }
 
     /**
